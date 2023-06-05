04ab8aa8841d382a2c16e43068675c375198d6f2
Harry <50802261+daWiz314@users.noreply.github.com>
2023-06-05 11:31:02 -0400

    Creating bare index.page

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<link rel="stylesheet" href="styles/main.css">
+<link rel="stylesheet" href="styles/index.css">
+
+<head>
+
+</head>
+
+<body class='no-scroll'>
+    <header>
+        <nav>
+            <ul>
+                <a href="">
+                    <li class='logo'></li>
+                </a><a href="">
+                    <li class='up'>Invitation</li>
+                </a>
+                <a href="">
+                    <li class='up'>RSVP</li>
+                </a>
+                <a href="">
+                    <li class='up'>Registry</li>
+                </a>
+                <a href="">
+                    <li class='up'>Our Story</li>
+                </a>
+                <a href="">
+                    <li class='up'>Wedding Party</li>
+                </a>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <div>
+            <h1 class='up text-shadow'>Harry & Rebecca</h1>
+            <h3 class='up text-shadow'>28 October 2023</h3>
+        </div>
+    </main>
+</body>
+
+</html>
