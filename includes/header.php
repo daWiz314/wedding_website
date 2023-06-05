d45532cfc8929b6a216382088124460aaf9fa37c
Harry <50802261+daWiz314@users.noreply.github.com>
2023-06-05 15:55:40 -0400

    Added actual page title to each page

--- includes/header.php
+++ includes/header.php
@@ -2,6 +2,6 @@
     echo '<link rel="stylesheet" href="styles/main.css">';
     echo '<meta name="viewport" content="width=device-width, initial-scale=1">';
     echo '<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">';
-    echo '<title>The Martin\'s Wedding - '. '</title>';
+    echo '<title>The Martin\'s Wedding - '. ucfirst($currentPage) .'</title>';
 
 ?>
