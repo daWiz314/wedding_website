c3409c7b64964b301b69c4752f136db529ee0be7
Harry <programmer6299@icloud.com>
2023-06-06 11:09:07 -0400

    Added small code to test dev env

---
+++ pages/invitation.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <?php   
+            $currentPage = "invitation";
+            $path = "./";
+            include_once "../includes/header.php";
+        ?>
+    </head>
+    <body>
+        <? include_once "../includes/nav.php"; ?>
+        Test
+    </body>
+</html>
