79629189536a47e9768e850be380e28c33bf7e9d
Harry <50802261+daWiz314@users.noreply.github.com>
2023-06-05 20:34:07 -0400

    Fixed nav menu so that it'll work on mobile

--- includes/nav.php
+++ includes/nav.php
@@ -1,6 +1,11 @@
 <nav>
+    <input type="checkbox" name="logo_checkbox" id="logo_checkbox">
+    <label for="logo_checkbox">
+        <img src="/imgs/logo.png" alt="Logo containing letters H and R" class="logo">
+    </label>
+    
     <ul>
-        <li><img src="/imgs/logo.png" alt="Logo containing letters H and R" class="logo"></li>
+        <li></li>
         <?php include_once 'nav_helper.php' ?>
     </ul>
 </nav>
