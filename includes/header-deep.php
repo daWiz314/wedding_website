396607030c9793a9b975ef32d80ce92b5e6e48e9
Harry <programmer6299@icloud.com>
2023-06-08 12:44:42 -0400

    Created deeper header to point to proper things

---
+++ includes/header-deep.php
@@ -0,0 +1,6 @@
+<?php
+    echo '<link rel="stylesheet" href="../styles/main.css">';
+    echo '<meta name="viewport" content="width=device-width, initial-scale=1">';
+    echo '<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">';
+    echo '<title>The Martin\'s Wedding - '. ucfirst($currentPage) .'</title>';
+?>
