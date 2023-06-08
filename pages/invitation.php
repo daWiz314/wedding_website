ad563ec9e210c83e081d8212a34a39eefc58334a
Harry <programmer6299@icloud.com>
2023-06-08 12:46:03 -0400

    Created page will update when I get envelope for it

--- pages/invitation.php
+++ pages/invitation.php
@@ -1,14 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <?php   
-            $currentPage = "invitation";
-            $path = "./";
-            include_once "../includes/header.php";
-        ?>
-    </head>
-    <body>
-        <? include_once "../includes/nav.php"; ?>
-        Test
-    </body>
+
+<head>
+    <?php
+    $currentPage = "invitation";
+    $path = "./";
+    include_once "../includes/header-deep.php";
+    ?>
+    <link rel="stylesheet" href="../styles/invitation.css">
+</head>
+
+<body class="no-scroll">
+    <?php include_once "../includes/nav.php"; ?>
+    <main>
+        <div>
+            <h1 class="up">
+                Invitation
+            </h1>
+            <span></span>
+            <h3 class="up">
+                Click to open!
+            </h3>
+        </div>
+    </main>
+</body>
+
 </html>
