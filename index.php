3c5842ea2ceb00a52203e8d84b5e46ffc59d602c
Harry <programmer6299@icloud.com>
2023-06-08 12:43:43 -0400

    Got rid of text-shadow class, didn't end up using it

--- index.php
+++ index.php
@@ -11,13 +11,13 @@
         include_once "./includes/header.php";
     ?>
 </head>
-
+ 
 <body class='no-scroll'>
     <?php include_once "./includes/nav.php" ?>
     <main>
         <div>
-            <h1 class='up text-shadow'>Harry & Rebecca</h1>
-            <h3 class='up text-shadow'>28 October 2023</h3>
+            <h1 class='up'>Harry & Rebecca</h1>
+            <h3 class='up'>28 October 2023</h3>
         </div>
     </main>
 </body>
