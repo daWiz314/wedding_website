bd567f542d52f13e65bc96db793d9912f84bedaf
Harry <programmer6299@icloud.com>
2023-06-08 12:45:30 -0400

    Sadly, added script to fix initial viewing of nav menu on mobile :( So much for all raw CSS

--- includes/nav.php
+++ includes/nav.php
@@ -3,9 +3,12 @@
     <label for="logo_checkbox">
         <img src="/imgs/logo.png" alt="Logo containing letters H and R" class="logo">
     </label>
-    
-    <ul>
-        <li></li>
+    <ul id="nav_list">
+    <script>
+        document.getElementById("nav_list").style.transform = "scale(0)";
+        console.log("It's fucking gone!");
+    </script>
         <?php include_once 'nav_helper.php' ?>
+
     </ul>
 </nav>
