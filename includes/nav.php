4b80fec486debf4e4738d0afe21d94ea7163df83
Harry <50802261+daWiz314@users.noreply.github.com>
2023-06-19 11:36:54 -0400

    Slight changes

--- includes/nav.php
+++ includes/nav.php
@@ -5,8 +5,10 @@
     </label>
     <ul id="nav_list">
     <script>
-        document.getElementById("nav_list").style.transform = "scale(0)";
+        document.getElementById("nav_list").style.scale = 0;
+        document.getElementById("nav_list").style.visibility = "hidden";
         console.log("It's fucking gone!");
+        console.log("Apparently it's not fucking gone");
     </script>
         <?php include_once 'nav_helper.php' ?>
 
