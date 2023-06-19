ed148e148873d5df2816260478e6e1ba824b0e95
Harry <50802261+daWiz314@users.noreply.github.com>
2023-06-19 12:54:10 -0400

    Updated for mobile Added z-index so it'll be above everything Add animation class so it doesn't animate unfill when browser first load

--- includes/nav.php
+++ includes/nav.php
@@ -1,14 +1,27 @@
 <nav>
     <input type="checkbox" name="logo_checkbox" id="logo_checkbox">
     <label for="logo_checkbox">
-        <img src="/imgs/logo.png" alt="Logo containing letters H and R" class="logo">
+        <img src="/imgs/logo.png" alt="Logo containing letters H and R" class="logo" id="logo_img">
     </label>
     <ul id="nav_list">
     <script>
-        document.getElementById("nav_list").style.scale = 0;
-        document.getElementById("nav_list").style.visibility = "hidden";
-        console.log("It's fucking gone!");
-        console.log("Apparently it's not fucking gone");
+        let nav_clicked = false;
+        
+        function checkIfSmall() {
+            if (document.body.clientWidth <= 1000) {
+                document.getElementById("nav_list").style.scale = 0;
+                document.getElementById("nav_list").style.visibility = "hidden";
+            }
+            if (document.body.clientWidth > 1000) {
+                document.getElementById("nav_list").style.scale = 1;
+                document.getElementById("nav_list").style.visibility = "visible";
+            }
+        }
+        document.getElementById("logo_img").addEventListener("click", function() {
+            document.getElementById("nav_list").classList.add("add-animation-unfill");
+        }, once = true)
+        window.addEventListener("resize", function() {checkIfSmall()});
+        checkIfSmall();
     </script>
         <?php include_once 'nav_helper.php' ?>
 
