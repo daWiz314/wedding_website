80510672b2ca042d2ccec389fdec716800c600dd
Harry <50802261+daWiz314@users.noreply.github.com>
2023-06-29 10:59:17 -0400

    Lots of changes as follows: Made GroupCheckBoxes static - This way we can use them on anything, it doesn't use anything required Fixed refs to GroupCheckBoes.toggle - Changed from this.toggle to GroupCheckBoxes.toggle Updated add_listener_to_pair - Actually toggles checkmarks - Does functions provided, atm they only display - - Will update that to be a function in the properties of the class - Toggles other box if one is unchecked Added function to display plus one elements Added test_check_boxes() which test to display on body load Commented out old code

--- scripts/rsvp.ts
+++ scripts/rsvp.ts
@@ -44,9 +44,10 @@ class GroupCheckboxes {
 
     /**
      * Toggles the checkbox state of an item
+     * Static so we can access it inside methods inside methods 0_o
      * @param item The actual item to toggle
      */
-    toggle(item:CheckboxPair) {
+    static toggle(item:CheckboxPair) : void {
         if (this.check_state(item)) {
             item.checkbox.checked = false;
         } else {
@@ -60,10 +61,10 @@ class GroupCheckboxes {
      * Only works if there are only 2 checkboxes in a group
      * @returns a Boolean based on if it ran or not
      */
-    toggle_pair() : boolean {
+    toggle_pair() : Boolean {
         if (this.otherItems === undefined) { // Make sure there are no other items
-            this.toggle(this.item1);
-            this.toggle(this.item2);
+            GroupCheckboxes.toggle(this.item1);
+            GroupCheckboxes.toggle(this.item2);
             return true; // It ran, and did it's thing
         } else {
             return false; // It did not run
@@ -76,7 +77,7 @@ class GroupCheckboxes {
      * @returns A Boolean based on if it's checked or not
      * 
      */
-    check_state(item:CheckboxPair) : boolean {
+    static check_state(item:CheckboxPair) : Boolean {
         return item.checkbox.checked;
     }
 
@@ -85,44 +86,87 @@ class GroupCheckboxes {
      * @param function1 Function for the first checkbox item in pair
      * @param function2 Function for the second checkbox item in pair
      */
-    add_listener_to_pair(function1: Function, function2: Function) {
+    add_listener_to_pair(function1: Function = ()=>void{}, function2: Function = ()=>void{}) {
         if (this.otherItems === undefined) {
-            this.item1.checkbox.addEventListener("change", function(this: GroupCheckboxes) {
-                this.toggle(this.item2);
-                function1();
+            var item1 = this.item1;
+            var item2 = this.item2;
+            this.item1.display.addEventListener("click", function(this: GroupCheckboxes) {
+                GroupCheckboxes.toggle(item1);
+                if (GroupCheckboxes.check_state(item1)) { // This way the user can uncheck the 'Y' and it will do stuff
+                    function1(true); // Display the items
+                    if (GroupCheckboxes.check_state(item2)) { // This way we can uncheck the 'N'
+                        GroupCheckboxes.toggle(item2);
+                    }
+                } else { // This is incase the user uncheck the 'Y', toggle the 'N'
+                    function1(false);
+                    GroupCheckboxes.toggle(item2);
+                }
             });
-            this.item2.checkbox.addEventListener("change", function(this: GroupCheckboxes) {
-                this.toggle(this.item1);
-                function2();
+            this.item2.display.addEventListener("click", function(this: GroupCheckboxes) {
+                GroupCheckboxes.toggle(item2); // Toggle button
+                if (GroupCheckboxes.check_state(item2)) { // Allow to uncheck 'N' and do stuff
+                    if (GroupCheckboxes.check_state(item1)) { // Make sure we do the opposite of what we are doing
+                        GroupCheckboxes.toggle(item1);
+                    }
+                    function2(false); // Undisplay the items
+                } else { // If we uncheck the 'N', toggle the 'Y'
+                    GroupCheckboxes.toggle(item1);
+                    function2(true);
+                }
             })
         }
     }
 }
 
-// Test to make sure it compiles
-
-console.log("Hello world");
-
-// -------------------- All below this is old code ----------------------------
-function display_pos() {
-    for (let obj in pos) {
+/**
+ * Display Plus One element input fields
+ * @param display A bool that will either display the items or not
+ */
+let display_po = function (display: Boolean) {
+    for(let obj in pos) { // The pos array will contain a few other elements, and if we try to do anything with them, it will just error out, allowing us to affect just the actual elements
         try {
-           (pos[obj] as HTMLElement).style.display = "unset";
-        }
-        catch {
+            if (display) {
+                (pos[obj] as HTMLElement).style.display = "unset";
+            } else {
+                (pos[obj] as HTMLElement).style.display = "none";
+            }
+        } catch {
             break;
         }
     }
 }
 
-function check_no() {
-    if ((po_no as HTMLInputElement).checked) {
-        (po_yes as HTMLInputElement).checked = false;
-    } else {
-        display_pos();
-    }
+// Test to make sure it compiles
+function test_check_boxes() {
+    let attending = new GroupCheckboxes(new CheckboxPair("at_yes", "at_yes_cb"), new CheckboxPair("at_no", "at_no_cb"));
+    let plus_one = new GroupCheckboxes(new CheckboxPair("po_yes", "po_yes_cb"), new CheckboxPair("po_no", "po_no_cb"));
+    attending.add_listener_to_pair(()=>void{}, display_po);
+    plus_one.add_listener_to_pair(display_po, display_po);
 }
+
+
+
+
+// -------------------- All below this is old code ----------------------------
+// function display_pos() {
+//     for (let obj in pos) {
+//         try {
+//            (pos[obj] as HTMLElement).style.display = "unset";
+//         }
+//         catch {
+//             break;
+//         }
+//     }
+// }
+
+// function check_no() {
+//     if ((po_no as HTMLInputElement).checked) {
+//         (po_yes as HTMLInputElement).checked = false;
+//     } else {
+//         display_pos();
+//     }
+// }
 // Force 'unwrapping' because those elements WILL exist on default
-var po_yes:HTMLElement = document.getElementById("po_yes")!;
-var po_no:HTMLElement = document.getElementById("po_no")!;
-po_yes.addEventListener("onclick", function() {check_no()});
+// var po_yes:HTMLElement = document.getElementById("po_yes")!;
+// var po_no:HTMLElement = document.getElementById("po_no")!;
+// po_yes.addEventListener("onclick", function() {check_no()});
