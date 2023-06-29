eda7a47392dcbce05c3a647d952a21b81abe8d71
Harry <50802261+daWiz314@users.noreply.github.com>
2023-06-29 10:54:46 -0400

    Added ids for js to find elements easier

--- pages/rsvp.php
+++ pages/rsvp.php
@@ -12,7 +12,7 @@
     <script src="../scripts/rsvp.js"></script>
 </head>
 
-<body class="no-scroll">
+<body class="no-scroll" onload="test_check_boxes()">
     <?php include_once "../includes/nav.php"; ?>
     <main>
         <div>
@@ -27,13 +27,13 @@
                     <fieldset required>
                         <label class="first">Y</label>
                         <label for="at_yes" class="first">
-                            <input type="checkbox" name="at_no" id="at_yes">
-                            <div class="check_box"></div>
+                            <input type="checkbox" name="at_yes" id="at_yes_cb">
+                            <div class="check_box" id="at_yes"></div>
                         </label>
                         <label>N</label>
                         <label for="at_no">
-                            <input type="checkbox" name="at_no" id="at_no">
-                            <div class="check_box"></div>
+                            <input type="checkbox" name="at_no" id="at_no_cb" checked>
+                            <div class="check_box" id="at_no"></div>
                         </label>
                         
                     </fieldset>
@@ -41,13 +41,13 @@
                     <fieldset required>
                         <label class="first">Y</label>
                         <label for="po_yes" class="first">
-                            <input type="checkbox" name="po_yes" id="po_yes">
-                            <div class="check_box"></div>
+                            <input type="checkbox" name="po_yes" id="po_yes_cb">
+                            <div class="check_box" id="po_yes"></div>
                         </label>
                         <label>N</label>
                         <label for="po_no">
-                            <input type="checkbox" name="po_no" id="po_no">
-                            <div class="check_box"></div>
+                            <input type="checkbox" name="po_no" id="po_no_cb" checked>
+                            <div class="check_box" id="po_no"></div>
                         </label>
                     </fieldset>
                     <label for="f_name" class="up">First Name</label>
