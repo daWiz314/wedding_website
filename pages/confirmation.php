099f037498f1952ffba07b79bca9625091179c97
Harry <50802261+daWiz314@users.noreply.github.com>
2023-07-07 14:08:19 -0400

    Created page

---
+++ pages/confirmation.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php 
+    $currentPage = "confirm";
+    $path = "./";
+    include_once "../includes/header-deep.php";
+    ?>
+</head>
+<body class="no-scroll">
+    <main>
+        
+    </main>
+    
+</body>
+</html>
