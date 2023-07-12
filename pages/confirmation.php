61d3cb1467e72400f12970a35a75eb979579cf9f
Harry <50802261+daWiz314@users.noreply.github.com>
2023-07-12 16:56:05 -0400

    Added base layout

--- pages/confirmation.php
+++ pages/confirmation.php
@@ -6,11 +6,27 @@
     $path = "./";
     include_once "../includes/header-deep.php";
     ?>
+    <link rel="stylesheet" href="../styles/confirmation.css">
 </head>
 <body class="no-scroll">
     <?php include_once "../includes/nav.php"; ?>
     <main>
-        
+        <div>
+            <h2 class="up">Thank you for submitting!</h2>
+        </div>
+        <span></span>
+        <div id="user_info_container">
+            <h3 class="up">Information submitted for: </h3>
+            <p id="attending">Attending?</p>
+            <p id="name">Name:</p>
+            <p id="email">Email:</p>
+            <p id="plus_one">Plus One?</p>
+            <p id="plus_one_name">Plus One Name:</p>
+        </div>
+        <span></span>
+        <div>
+            <h3 id="email_sent">An email has been sent to </h3>
+        </div>
     </main>
 </body>
 </html>
