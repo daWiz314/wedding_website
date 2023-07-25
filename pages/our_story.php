84897694849403db70b34b9285af26ee407ff957
Harry <50802261+daWiz314@users.noreply.github.com>
2023-07-25 11:07:05 -0400

    Created bare bones page

---
+++ pages/our_story.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php
+            $currentPage = "our_story";
+            $path = "./";
+            include_once "../includes/header-deep.php";
+        ?>
+    </head>
+    <body>
+        <?php include_once "../includes/nav.php"; ?>
+        <main>
+            <div>
+                <p>Still a work in progress!</p>
+            </div>
+        </main>
+    </body>
+</html>
---
+++ pages/registry.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php
+            $currentPage = "registry";
+            $path = "./";
+            include_once "../includes/header-deep.php";
+        ?>
+    </head>
+    <body>
+        <?php include_once "../includes/nav.php"; ?>
+        <main>
+            <div>
+                <p>Still a work in progress!</p>
+            </div>
+        </main>
+    </body>
+</html>
