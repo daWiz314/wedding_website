738b349ac03b582875dbef73affd34851195ccaf
Harry <50802261+daWiz314@users.noreply.github.com>
2023-07-26 12:07:05 -0400

    Had to add function toggle_size - Otherwise the nav in mobile takes up the whole screen not allowing users to click

--- includes/nav.php
+++ includes/nav.php
@@ -1,4 +1,4 @@
-<nav>
+<nav id="nav">
     <input type="checkbox" name="logo_checkbox" id="logo_checkbox">
     <label for="logo_checkbox">
         <img src="/imgs/logo.png" alt="Logo containing letters H and R" class="logo" id="logo_img">
@@ -7,10 +7,32 @@
     <script>
         let nav_clicked = false;
         
+        function toggle_size() { // We have to do this if the user is on mobile, this way it doesn't take up the whole screen with it's width and height. When it does take up the whole screen, it blocks the user from interacting.
+            if (nav_clicked) {
+                document.getElementById("nav_list").style.width = 'min-content';
+                document.getElementById("nav_list").style.height = 'min-content';
+            } else {
+                document.getElementById("nav_list").style.width = 0;
+                document.getElementById("nav_list").style.height = 0;
+            }
+        }
+
         function checkIfSmall() {
             if (document.body.clientWidth <= 1000) {
                 document.getElementById("nav_list").style.scale = 0;
                 document.getElementById("nav_list").style.visibility = "hidden";
+                document.getElementById("nav_list").style.width = 0;
+                document.getElementById("nav_list").style.height = 0;
+                document.getElementById("logo_img").addEventListener("click", function() {
+                    if (nav_clicked) {
+                        nav_clicked = false;
+                        toggle_size();
+                    } else {
+                        nav_clicked = true;
+                        toggle_size();
+                    }
+                })
+
             }
             if (document.body.clientWidth > 1000) {
                 document.getElementById("nav_list").style.scale = 1;
