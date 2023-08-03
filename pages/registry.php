3db4a27ba9cc3bbac16ee12da058c6e3e29714e2
dawiz314 <programmer6299@icloud.com>
2023-08-03 13:19:40 -0400

    Updated to actually have registry link

--- pages/registry.php
+++ pages/registry.php
@@ -12,7 +12,19 @@
         <?php include_once "../includes/nav.php"; ?>
         <main>
             <div>
-                <p>Still a work in progress!</p>
+                <h1>Registry</h1>
+                <span></span>
+                <p>
+                    The most important thing is to have you with us to celebrate!
+                </p>    
+                <p>
+                    No gifts are needed or expected; however, we have been asked what we need or would like.
+                </p>
+                <p>
+                    A monetary contribution towards our future home would make our day. If you prefer to purchase a gift, we have also registered below.
+                </p>
+                <h1>Amazon</h1>
+                <a href="https://www.amazon.com/wedding/rebecca-martin-harry-martin--october-2023/registry/11ITZJ6QSWHQP">Amazon Registry Link</a>
             </div>
         </main>
     </body>
