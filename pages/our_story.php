6a7ab6ec74c2229c27e51abf32f85858a96a4e5e
dawiz314 <programmer6299@icloud.com>
2023-08-03 13:19:28 -0400

    Updated actual story, need to add pictures

--- pages/our_story.php
+++ pages/our_story.php
@@ -10,10 +10,20 @@
     </head>
     <body>
         <?php include_once "../includes/nav.php"; ?>
+        <div>
+            <h1>Our Story</h1>
+        </div>
         <main>
             <div>
-                <p>Still a work in progress!</p>
-                
+                <p>
+                    We met online in 2020 while Harry was home on Christmas leave after having surgery. Going into the new year, 2021, we continued talking until the distance without us meeting became difficult. We got back in touch a few months later and began talking again. Harry was in injury rehab and was only given his phone on the weekends - if he was lucky. We continued this for about 6 months until it became apparent that it was not going to work. 
+                </p>
+                <p>
+                    Fast forward to 2022, Harry texted Becca after 6 months of not being in contact. The wildest part is that Becca had a dream a week before about seeing Harry. This being said, she saw the text when she woke up and rolled back over because she thought it was another dream. We reconnected and clicked like we had during our first conversation. Becca finally had the chance to fly down to Texas to meet Harry. We immediately felt at home and comfortable with each other. The weekend allowed us to truly connect face to face instead of our normal daily facetime calls. Becca flew home and Harry flew out to Korea a few days after. The distance and time-difference was challenging at times, but we managed to still have movie date nights, almost a daily call, and continue to grow together. 
+                </p>
+                <p>
+                    7 months later, Harry finally comes back to Texas and Becca flew down as soon as she possibly could to see him. We continued the cycle of Becca flying down every month or so to see Harry. Long story short, Becca is a pro-lone traveler now. Thanksgiving weekend was the weekend Harry proposed to Becca. We decided to elope in February, on our year anniversary. So, that is exactly what we did! We held a small ceremony in the backyard of an Airbnb. Becca's immediate family and best friend were able to fly down and we made sure to facetime Harry's family as well. The next time Becca flew down, she was finally able to book a one-way flight. That weekend, we drove back to Indiana to start our lives together as the Martins.
+                </p>
             </div>
         </main>
     </body>
